e63b19888d7ed95fe25446d331f067e67012f720
Peter Dolan <dolan118@morris.umn.edu>
2018-10-26 01:39:17 -0500

    Very, very large number of updates.. vignettes, source code, package details, help files, etc.

--- src/dtou.cpp
+++ src/dtou.cpp
@@ -3,28 +3,28 @@ using namespace Rcpp;
 
 extern void rc(char *str);
 
+/* Global variables are used to minimize memory requirements incurred by using recursion */
 char *S1;        // Global character array containing String imported from R
-long *I1;        // Global Stack
-long *dtou;      // Global array that will hold the results
-long i,j,k,next; //We make them global so space isn't wasted inside the recursion routine.
+long *I1;        // Global array that will emulate the stacks used in the algorithm
+long *dtou;      // Global array that will hold the distance to uniqueness
+long i,j,k,next; // Loop dummy variables
 long d;          // A depth cutoff [only used by depth-limited version]
-long l, m;       // globals used by the stack-size variant
+long l, m;       // More dummy variables used by the stack-size variant
 long _stringLength=0; // A global used to keep track of the size (change based on whether RC is being analyzed or not)
-bool verbose;
+bool verbose;   // Currently unused
 
 //Global Initialization:
 List convertRStringsandGlobalSetup(std::vector<std::string> RString, bool parseRC, int depth){
 	/*
-	* Converts list of Rstrings into single X-delimted string stored in global S1
+	* Converts list of Rstrings into single X-delimited string stored in global S1
 	* Also prepares an R List to hold the results of the calculation
 	*      the int depth is only used by some of the function calls
-	* SideEffects: Intializes String S1
-	*            : Sets n to the appropriate value
-	*            :
+	* Side Effects: Intializes String S1
+	*             : Sets n to the appropriate value
+	*             :
 	*/
 
-
-	long unsigned n,i,j,k,tmp,n1;
+	long unsigned n,i,j,k,tmp,n1;             // Only run once so we can use local variables
 	long unsigned n_strings=RString.size();
 
 	List results=List::create();
@@ -67,20 +67,21 @@ List convertRStringsandGlobalSetup(std::vector<std::string> RString, bool parseR
 
 	// Initialize Array encoding virtual tree of stacks:
 	for(i=0;i<n1;i++){I1[i]=i+1;}
-	I1[n1]=-1;
+	I1[n1-1]=-1; // The terminating 'X' should point nowhere
+	I1[n1]=-1;   // A probably unnecessary safety precaution
 
 	d=depth;
-	_stringLength=n1;
+	_stringLength=n1;    // Set global value so everybody knows how long he string is
 	return(results);
 }
 
-void rc(char *str){
+void rc(char *str){   // Calculates the reverse complement of a string
 	char tmp;
 	unsigned long n,i,j;
 
 	n = strlen(str);
 
-	i=0;              // Left end
+	i=0;               // left end
 	j=n-1;             // right end
 	while(i<=j){
 		tmp=str[i];     // store the left end
@@ -121,6 +122,22 @@ void rc(char *str){
 }
 
 // The workhorses-- there is a LOT of code redundancy-- but in the interests of efficiency I am not adding conditionals
+// In all these function variables are as follows:
+//     S1 is the string being analyzed
+//     I1 is the global array holding all the stacks
+//     Ip1 is the index to the stack being processed.  Refers to I1
+//     A1 is the top of the A-stack
+//     C1 is the top of the C-stack
+//     G1 is the top of the G-stack
+//     T1 is the top of the T-stack
+//     The dummy variable i+depth indicates the character in S1 currently being examined
+//  Specialized variables
+//     In depth-limited variants, d indicates the max-depth allowed for the recursion
+//     In speed-optimized variants, the stack-size is a parameter indicating, in advance the size of the stack
+//                                  this allows the program to switch to a more efficient comparision when the
+//                                  stack has 2 elements.
+
+
 void recurse(long Ip1, long depth){
 	long A1=-1, C1=-1, G1=-1, T1=-1;
 	if(Ip1==-1){
@@ -155,6 +172,11 @@ void recurse(long Ip1, long depth){
 			I1[i]=-1;
 			dtou[i]=depth;
 			break;
+		case '\0': // This catches the end of string character
+			break;
+		default:
+			Rcout << "Location:  " << i << " depth: " << depth << " value: "<< S1[i+depth] << " String length " << _stringLength << "\n";
+			stop("Unrecognized character in string\n");
 		}
 		i=next;
 	}
@@ -202,6 +224,11 @@ void depthLimitedRecurse(long Ip1, long depth){
 			I1[i]=-1;
 			dtou[i]=depth;
 			break;
+		case '\0': // This catches the end of string character
+			break;
+		default:
+			Rcout << "Location:  " << i << " depth: " << depth << " value: "<< S1[i+depth] << " String length " << _stringLength << "\n";
+		stop("Unrecognized character in string\n");
 		}
 		i=next;
 	}
@@ -227,11 +254,12 @@ void recurseWithSize(long Ip1, long depth,long stackSize){
 		l = Ip1+depth;
 		m = I1[Ip1]+depth;
 
-		while(S1[l]==S1[m]){
+		while(S1[l]==S1[m]&&S1[l]!='X'){
 			l++;m++;
 		} ; // Run until they are different... assumption is that we will NOT cross 'X's
 		// That might not be warranted-- I'll try it without the safeguard first
 		// And then see what kind of performance hit I get if I scan for 'X'
+		// Definitely necessary.  First thing I ran it on had this problem.
 		dtou[Ip1]=l-Ip1+1;
 		if(S1[l]=='X'){dtou[Ip1]--;}
 
@@ -271,6 +299,11 @@ void recurseWithSize(long Ip1, long depth,long stackSize){
 			I1[i]=-1;
 			dtou[i]=depth;
 			break;
+		case '\0': // This catches the end of string character
+			break;
+		default:
+			Rcout << "Location:  " << i << " depth: " << depth << " value: "<< S1[i+depth] << " String length " << _stringLength << "\n";
+		  stop("Unrecognized character in string\n");
 		}
 		i=next;
 	}
@@ -295,7 +328,7 @@ void depthLimitedRecurseWithSize(long Ip1, long depth,long stackSize){
 		l = Ip1+depth;
 		m = I1[Ip1]+depth;
 
-		while(l-Ip1 <= d && S1[l]==S1[m]){
+		while(l-Ip1 <= d && S1[l]==S1[m] &&S1[l]!='X'){
 			l++;m++;
 		} ; // Run until they are different... assumption is that we will NOT cross 'X's
 		// That might not be warranted-- I'll try it without the safeguard first
@@ -340,6 +373,11 @@ void depthLimitedRecurseWithSize(long Ip1, long depth,long stackSize){
 			I1[i]=-1;
 			dtou[i]=depth;
 			break;
+		case '\0': // This catches the end of string character.  Should not happen
+			break;
+		default:
+			Rcout << "Location:  " << i << " depth: " << depth << " value: "<< S1[i+depth] << " String length " << _stringLength << "\n";
+		  stop("Unrecognized character in string\n");
 		}
 		i=next;
 	}
@@ -418,7 +456,6 @@ List c_dtouS2DepthLimit(std::vector<std::string> RString, bool rc,long depth){ /
 
 	return results;
 }
-
 
 
 // You can include R code blocks in C++ files processed with sourceCpp
